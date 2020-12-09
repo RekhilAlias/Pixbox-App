baf73fb1d999abc50935acc77cbb0fc947e58bbb
Rekhil elias paulose <rekhilelias2014@gmail.com>
2020-12-09 10:54:33 +0530

    Add files via upload

---
+++ precache-manifest.c3f5a76362c32d405e87c3eeb1a397bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f25aecd9037c0cccf782eb8013fe0d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d46ef0cc3cefbcdd2c5",
+    "url": "/static/css/main.ee69d4ea.chunk.css"
+  },
+  {
+    "revision": "8ac21b32d6b7daf76c2e",
+    "url": "/static/js/2.3289fda5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3289fda5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d46ef0cc3cefbcdd2c5",
+    "url": "/static/js/main.83c5a87c.chunk.js"
+  },
+  {
+    "revision": "386a7618cc9a57a2c09f",
+    "url": "/static/js/runtime-main.8e663b6d.js"
+  }
+]);
